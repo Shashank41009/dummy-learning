809ad8fe7703702219e2961273daba0b95d3a5ef
shashank <shashankangadi75@gmail.com>
2026-01-07 12:10:37 +0530

    sub added
    Signed-off-by: shashank <shashankangadi75@gmail.com>

--- calculator.py
+++ calculator.py
@@ -3,5 +3,10 @@ class Calculator:
 		pass
 	def add(self, *values):
 		return sum(values)
+	def subtract(self, *values):
+		return values[0] - values[1]
 cal = Calculator()
 print(cal.add(1,2))
+print(cal.subtract(1,2)) #subtract added
+
+
