be14f06039cb76219397f8d300546430c03628aa
shashank <shashankangadi75@gmail.com>
2026-01-07 13:09:31 +0530

    new modification
    Signed-off-by: shashank <shashankangadi75@gmail.com>

--- calculator.py
+++ calculator.py
@@ -8,8 +8,20 @@ class Calculator:
 		for value in values[1:]:
 			total -= value
 		return total
+	def multiply(self, *values):
+		total = values[0]
+		for value in values[1:]:
+			total *= value
+		return total
+	def divide(self, *values):
+		total = values[0]
+		for value in values[1:]:
+			total /= value
+		return total
 cal = Calculator()
-print(cal.add(1,2))
-print(cal.subtract(1,2)) #subtract added
+print(cal.add(1,2,3))
+print(cal.subtract(1,2,3)) #subtract added
+print(cal.multiply(1,2))
+print(cal.divide(2,1))
 
 
